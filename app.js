397adaa4c092d688575297b0b15140e0151f7e86
Mridul Maurya <mmridul934@gmail.com>
2021-09-05 15:49:58 -0700

    console basics

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('Hey, This is my first piecec of js code!!!')
+
+console.log(225487854)
+
+console.log(true)
+
+console.log(false,'string',2515478,{name:"Mridul"})
+
+console.warn("This is a warning")
+console.error("Do not distrub")
+
