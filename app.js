b9f476fd05b99a2ec0aad0cdbaef5ab802e49b17
Mridul Maurya <mmridul934@gmail.com>
2021-09-11 20:23:50 -0700

    Assignment Operators

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
-// Arithmetic Operators
+// Assignment Operators
 
-let specific_name="Mridul"
-console.log(specific_name)
+let Name = "Megha"
+let a = 25
+console.log(Name)
+console.log(a)
 
-const a=5
-const b=3
-console.log(a+b)
-console.log(a-b)
-console.log(a*b)
-console.log(a/b)
-console.log(a%b)
-console.log(a**b)
-
-let n=2  // Increment 
-n**=2
-console.log(n)
-
-n=3
-n*=3
-console.log(n)
-
-const x='5'  // Concatenation
-const y='2'
-console.log(x + y)
 
 
 
