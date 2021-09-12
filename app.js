990a0073b2911bcb72b0b4d56b657462b41da9c6
Mridul Maurya <mmridul934@gmail.com>
2021-09-11 20:15:42 -0700

    Arithmetic Operators

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
-//Data types
-//Primitive and Non-Prmimtive Datatypes
+// Arithmetic Operators
 
-var Name = "Mridul"
-Name = ""
-console.log(Name)
+let specific_name="Mridul"
+console.log(specific_name)
 
-let age = 18;
-console.log(age)
-console.log(typeof age)
+const a=5
+const b=3
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a**b)
 
-let brand = null
-console.log(brand)
-console.log(typeof undefined)
+let n=2  // Increment 
+n**=2
+console.log(n)
+
+n=3
+n*=3
+console.log(n)
+
+const x='5'  // Concatenation
+const y='2'
+console.log(x + y)
 
 
-let arr = [1,2,3,'string',true]
-arr.push('javascript')
-arr.push(663)
-arr.unshift('hello')
-console.log(arr)
-console.log(typeof arr)
 
